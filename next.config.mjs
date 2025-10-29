3aa8719b98bb0f36680236a69ff7052f354d59b8
Rodrigo Lopez <rodrigoloh@Air-de-Rodrigo.localdomain>
2025-10-29 14:15:20 -0600

    fix: elimina redirect de /nosotros -> / para habilitar la sección Nosotros

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async redirects() {
-    return [
-      { source: '/nosotros', destination: '/', permanent: true },
-    ]
-  },
 };
 export default nextConfig;
