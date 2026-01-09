b89df139c237236a328040ab5d88f5c070ff1d17
Rodrigo Lopez <rodrigoloh@Air-de-Rodrigo.localdomain>
2026-01-09 13:11:15 -0600

    Center home intro parallax content

--- components/HomeIntroParallax.tsx
+++ components/HomeIntroParallax.tsx
@@ -10,18 +10,18 @@ export default function HomeIntroParallax() {
     offset: ['start end', 'end start'],
   })
 
-  const dotY = useTransform(scrollYProgress, [0, 1], ['30px', '-70px'])
+  const dotY = useTransform(scrollYProgress, [0, 1], ['0px', '-80px'])
   const dotX = useTransform(scrollYProgress, [0, 1], ['0px', '-30px'])
-  const line1Y = useTransform(scrollYProgress, [0, 1], ['10px', '-50px'])
+  const line1Y = useTransform(scrollYProgress, [0, 1], ['0px', '-40px'])
   const line1X = useTransform(scrollYProgress, [0, 1], ['0px', '20px'])
-  const line2Y = useTransform(scrollYProgress, [0, 1], ['40px', '-90px'])
+  const line2Y = useTransform(scrollYProgress, [0, 1], ['0px', '-70px'])
   const line2X = useTransform(scrollYProgress, [0, 1], ['0px', '-10px'])
-  const dashY = useTransform(scrollYProgress, [0, 1], ['60px', '-110px'])
+  const dashY = useTransform(scrollYProgress, [0, 1], ['0px', '-90px'])
   const dashX = useTransform(scrollYProgress, [0, 1], ['0px', '30px'])
 
   return (
-    <section ref={ref} id="intro" className="section bg-white text-black relative overflow-hidden">
-      <div className="mx-auto max-w-5xl px-6 py-20 md:py-28 min-h-[42vh] md:min-h-[50vh] flex flex-col items-center justify-center text-center">
+    <section ref={ref} id="intro" className="section bg-white text-black relative overflow-hidden flex items-center justify-center">
+      <div className="mx-auto max-w-5xl px-6 w-full flex flex-col items-center justify-center text-center">
         <motion.div style={{ y: dotY, x: dotX }} className="mb-6 md:mb-8">
           <div className="h-7 w-7 md:h-10 md:w-10 rounded-full bg-cobaltBase" />
         </motion.div>
