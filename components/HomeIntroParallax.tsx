bab1877a912d4206735497f68fe4f171438e77ac
Rodrigo Lopez <rodrigoloh@Air-de-Rodrigo.localdomain>
2026-01-09 13:31:15 -0600

    Keep home intro text static

--- components/HomeIntroParallax.tsx
+++ components/HomeIntroParallax.tsx
@@ -12,10 +12,6 @@ export default function HomeIntroParallax() {
 
   const dotY = useTransform(scrollYProgress, [0, 1], ['0px', '-80px'])
   const dotX = useTransform(scrollYProgress, [0, 1], ['0px', '-30px'])
-  const line1Y = useTransform(scrollYProgress, [0, 1], ['0px', '-40px'])
-  const line1X = useTransform(scrollYProgress, [0, 1], ['0px', '20px'])
-  const line2Y = useTransform(scrollYProgress, [0, 1], ['0px', '-70px'])
-  const line2X = useTransform(scrollYProgress, [0, 1], ['0px', '-10px'])
   const dashY = useTransform(scrollYProgress, [0, 1], ['0px', '-90px'])
   const dashX = useTransform(scrollYProgress, [0, 1], ['0px', '30px'])
 
@@ -26,19 +22,13 @@ export default function HomeIntroParallax() {
           <div className="h-7 w-7 md:h-10 md:w-10 rounded-full bg-cobaltBase" />
         </motion.div>
 
-        <motion.h2
-          style={{ y: line1Y, x: line1X }}
-          className="text-3xl md:text-5xl lg:text-6xl font-bold leading-tight text-black"
-        >
+        <h2 className="text-3xl md:text-5xl lg:text-6xl font-bold leading-tight text-black">
           Brands that speak clearly.
-        </motion.h2>
+        </h2>
 
-        <motion.h2
-          style={{ y: line2Y, x: line2X }}
-          className="mt-4 text-3xl md:text-5xl lg:text-6xl font-bold leading-tight text-black"
-        >
+        <h2 className="mt-4 text-3xl md:text-5xl lg:text-6xl font-bold leading-tight text-black">
           Brands that <span className="text-cobaltBase">feel alive.</span>
-        </motion.h2>
+        </h2>
 
         <motion.div style={{ y: dashY, x: dashX }} className="mt-8 md:mt-10 h-[3px] w-14 bg-black" />
       </div>
